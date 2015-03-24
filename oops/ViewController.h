76c5818f58dd5a151665778eaedfd9df2db262d9
sean <zerojetlag@gmail.com>
2015-03-24 21:52:30 +0800

    Initial Commit

---
+++ oops/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oops
+//
+//  Created by sean on 15-3-24.
+//  Copyright (c) 2015年 sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ oops/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  oops
+//
+//  Created by sean on 15-3-24.
+//  Copyright (c) 2015年 sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
