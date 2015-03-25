6379d59699642a391e8e236fd5c32ae1bf4e6168
sean <zerojetlag@gmail.com>
2015-03-25 16:33:35 +0800

    root view

---
+++ oops/OSRootViewController.h
@@ -0,0 +1,14 @@
+//
+//  OSRootViewController.h
+//  oops
+//
+//  Created by sean on 15-3-25.
+//  Copyright (c) 2015年 sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OSRootViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@end
---
+++ oops/OSRootViewController.m
@@ -0,0 +1,37 @@
+//
+//  OSRootViewController.m
+//  oops
+//
+//  Created by sean on 15-3-25.
+//  Copyright (c) 2015年 sean. All rights reserved.
+//
+
+#import "OSRootViewController.h"
+
+@interface OSRootViewController ()
+
+@end
+
+@implementation OSRootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
