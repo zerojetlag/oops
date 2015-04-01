0df6260dbecb9ac5e21026491a3d84dd0287d6b1
sean <zerojetlag@gmail.com>
2015-04-01 21:12:30 +0800

    animated

--- oops/OSRootViewController.m
+++ oops/OSRootViewController.m
@@ -81,9 +81,19 @@
 }
 
 - (void) setEditing:(BOOL)editing animated:(BOOL) animated{
-    [super setEditing:editing animated:animated];
-    [self.tableView setEditing:editing animated:animated];
-    [self.tableView reloadData];
+    if (editing != self.editing) {
+        [super setEditing:editing animated:animated];
+        [self.tableView setEditing:editing animated:animated];
+        NSArray *indexes = [NSArray arrayWithObject:[NSIndexPath indexPathForRow:cities.count inSection:0]];
+        if (editing == YES) {
+            [self.tableView insertRowsAtIndexPaths:indexes withRowAnimation:UITableViewRowAnimationLeft];
+        } else {
+            [self.tableView deleteRowsAtIndexPaths:indexes withRowAnimation:UITableViewRowAnimationLeft];
+        }
+    }
+//    [super setEditing:editing animated:animated];
+//    [self.tableView setEditing:editing animated:animated];
+//    [self.tableView reloadData];
 }
 
 - (UITableViewCellEditingStyle)tableView:(UITableView *)tv
